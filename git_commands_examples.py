6f40f98212cb31c540fa60dae6feb9a0ea4c23ca
tudorobretin <tudorobre@gmail.com>
2022-03-31 12:52:15 +0300

    Changed README.md; uploaded test file ceva.py; uploaded a list of git commands
    some description

---
+++ git_commands_examples.py
@@ -0,0 +1,15 @@
+# cd -> change directory
+#
+# cd .\MAXMEDIA -> enters MAXMEDIA directory
+#
+# ls -> lists contents of current directory
+#
+# cat a.txt -> prints a.txt to console
+#
+# dir -Force ->this shows hidden files in current directory
+#
+# git clone "repo ssh url" -> this clones a git repo to your local machine
+#
+# git add . -> starts tracking all files
+#
+# git status -> tells status of files (ready to commit, not staged
